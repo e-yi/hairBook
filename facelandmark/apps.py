034bcf4210e33df6a43c1352d35bfe3c768b6493
e-yi <e_yi@foxmail.com>
2019-02-14 17:18:08 +0800

    delete app facelandmark and update pipfile

--- facelandmark/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FacelandmarkConfig(AppConfig):
-    name = 'facelandmark'
--- hairBook/urls.py
+++ hairBook/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from facelandmark.views import getFaceLandmarks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('faceLandmarks/', getFaceLandmarks),
 ]
