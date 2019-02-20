66f84d7876b7be68649b776681efd6d16096308e
e-yi <e_yi@foxmail.com>
2019-02-20 18:55:59 +0800

    恢复非随机的脸型检测

--- FHModel/views.py
+++ FHModel/views.py
@@ -19,12 +19,11 @@ logger = logging.getLogger('FHModel')
 @api_view(['POST'])
 def faceShape(request):
     try:
-        # img64 = request.data['face']
-        # img = base64.b64decode(img64)
-        # imageBytes = io.BytesIO(img)
-        # shape = get_face(imageBytes)
+        img64 = request.data['face']
+        img = base64.b64decode(img64)
+        imageBytes = io.BytesIO(img)
+        shape = get_face(imageBytes)
         # 为了方便测试将脸型改为随机
-        shape = random.choice(['方脸', '圆脸', '心形脸', '长脸', '椭圆脸', '梨形脸', '菱形脸'])
         assert len(shape) == 1
     except Exception as e:
         logger.debug(str(e))
