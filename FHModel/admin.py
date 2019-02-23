a8acac29a9c3dcaa7299bd0ee0ee2998122ceeab
e-yi <e_yi@foxmail.com>
2019-02-23 16:40:28 +0800

    完成admin界面

--- FHModel/admin.py
+++ FHModel/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
 
-admin.site.register(Face)
-admin.site.register(Hairstyle)
-admin.site.register(Hair)
+
+class FaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'faceType', 'hairstyle')
+
+    def hairstyle(self, obj):
+        return [hairstyle.hairstyle for hairstyle in obj.hairstyles.all()]
+
+    hairstyle.short_description = '适合发型'
+
+class HairstyleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hairstyle', 'curlDegree', 'heatDegree', 'get_faceType')
+
+    def get_faceType(self, obj):
+        return [face.faceType for face in obj.faceType.all()]
+
+    get_faceType.short_description = '适合脸型'
+
+class HairAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hairstyle', 'length', 'gender', 'heatCount', 'pictureURL')
+
+    def pictureURL(self, obj):
+        return settings.PIC_URL + obj.filename
+
+    pictureURL.short_description = '图片URL'
+
+admin.site.register(Face, FaceAdmin)
+admin.site.register(Hairstyle, HairstyleAdmin)
+admin.site.register(Hair, HairAdmin)
